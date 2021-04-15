465802ad69959d3626830fcb2817770dd73d7c3b
Nico Hille <nicohille@localhost.localdomain>
2021-04-15 17:36:05 +0200

    block_ssh_attempts.py

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
 
-import os
+import fwblock
 
+logfile=open("sshdlog","r")
 
-for line in open('/home/nicohille/Downloads/sshdlog'):
-    if "invalid user" in line:
-        print(line)
+outputUsers = []
+ipaddresses = []
 
+for line in logfile.readlines():
+    if "invalid user" in line: outputUsers.append(line)
 
+for line in outputUsers:
+    listLines=line.split(" ")
+    ip=listLines[-4]
+    ipaddresses.append(ip)
+
+UniqueIps=set(ipaddresses)
+
+for a in UniqueIps:
+    counter=0
+    for b in ipaddresses:
+        if a==b:
+            counter+=0
+
+    if counter>=3:
+        fwblock.block.ip(a)
+        print("Het ip address" + a + "kwam" + counter + "keer voor en wordt geblokeerd")
